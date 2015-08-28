e2e72c1cdd0bf696b3cf050643ba5dcb1c585c49
soundstorm <soundstorm.mail@gmail.com>
2015-08-28 19:48:09 +0300

    personnel login

--- handlers/personnel.js
+++ handlers/personnel.js
@@ -1,15 +1,52 @@
 var mongoose = require('mongoose');
-var Personnel = function (models) {
+var Personnel = function (db) {
     var _ = require('../node_modules/underscore');
     var personnelSchema = mongoose.Schemas['personnel'];
+    var crypto = require('crypto');
 
     this.login = function (req, res, next) {
         var session = req.session;
+        var body = req.body;
+        var email = body.email;
+        var pass = body.pass;
+        var shaSum = crypto.createHash('sha256');
 
-        if (session && session.loggedIn && session.lastDb) {
-            this.getUserById(req, res, next);
+        var personnelModel = db.model('personnels', personnelSchema);
+        var query;
+
+        var lastAccess;
+
+        shaSum.update(pass);
+
+        if (login && pass) {
+            query = personnelModel.findOne({
+                email: email
+            });
+            query.exec(function (err, personnel) {
+                if (err) {
+                    return next(err);
+                }
+
+                if (personnel.pass === shaSum.digest('hex')) {
+                    session.loggedIn = true;
+                    session.uId = personnel._id;
+                    session.uName = personnel.login;
+                    lastAccess = new Date();
+                    session.lastAccess = lastAccess;
+
+                    personnelModel.findByIdAndUpdate(personnel._id, {$set: {lastAccess: lastAccess}}, function (err, result) {
+                        if (err) {
+                            return next(err);
+                        }
+                    });
+
+                    res.status(200).send();
+                } else {
+                    res.status(403).send();
+                }
+            })
         } else {
-            return res.status(401).send();
+            res.status(400).send();
         }
     };
 
