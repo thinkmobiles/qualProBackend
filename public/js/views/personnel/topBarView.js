05f0172d9d01a80d0a510f862ba234bacb0b22ce
soundstorm <soundstorm.mail@gmail.com>
2015-08-31 19:22:56 +0300

    topBar Template + view start

--- public/js/views/personnel/topBarView.js
+++ public/js/views/personnel/topBarView.js
@@ -1,3 +1,33 @@
-/**
- * Created by roma on 31.08.15.
- */
+define([
+        'text!templates/personnel/topBarTemplate.html',
+        'custom',
+        'common'
+    ],
+    function (topBarTemplate, custom, Common) {
+        var TopBarView = Backbone.View.extend({
+            el:'#topBarHolder',
+            contentType: "personnel",
+            actionType: null, //Content, Edit, Create
+            template: _.template(topBarTemplate),
+
+            events:{
+                /*"click a.changeContentView": 'changeContentViewType',
+                "click ul.changeContentIndex a": 'changeItemIndex',
+                "click #top-bar-deleteBtn": "deleteEvent",
+                "click #top-bar-discardBtn": "discardEvent",
+                "click #top-bar-editBtn": "editEvent",
+                "click #top-bar-createBtn": "createEvent"*/
+            },
+
+            render: function(){
+                $('title').text(this.contentType);
+                var viewType = custom.getCurrentVT();
+
+                this.$el.html(this.template({ viewType: viewType, contentType: this.contentType}));
+
+                return this;
+            },
+        });
+
+        return TopBarView;
+    });
