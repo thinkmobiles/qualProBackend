e9924cab4502c18c39bfd3e40b550a2160415788
soundstorm <soundstorm.mail@gmail.com>
2015-08-31 19:30:09 +0300

    ++++

--- tests/module.inserter.test.js
+++ tests/module.inserter.test.js
@@ -9,7 +9,7 @@ describe("BDD for modules creator for qualPro", function () {
     it("Creating modules:", function (done) {
         this.timeout(15000);
 
-       /* moduleCreator.create(function(err, resp){
+        /*moduleCreator.create(function(err, resp){
             if(err){
                 return done(err);
             }
